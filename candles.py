0e0667ed1435367734edf7ef6cfc7902c812dbc4
morgenwecks <morgenwecks@outlook.com>
2018-08-07 11:14:32 +0800

    first attempt
    i just realized i tend to do things in the good old times way, the non-pythonic way, and could use some practice here.

---
+++ candles.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the birthdayCakeCandles function below.
+def birthdayCakeCandles(ar):
+    max = 0
+    maxlist = []
+    for candle in ar:
+        if candle >= max:
+            max = candle
+    for candle in ar:
+        if candle == max:
+            maxlist.append(candle)
+    return(len(maxlist))
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
